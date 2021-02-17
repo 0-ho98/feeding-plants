ec45ddb5f60d6bdb38efb82122ee4f7bd6798345
(youngho) <one0374@naver.com>
2021-02-17 16:06:39 +0900

    클라이언트 부분만 빌드해서 올려놨음

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,29 +1,35 @@
 import React, { useState } from "react";
-import {Route, BrowserRouter, Switch} from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import AddingPlants from "routes/AddingPlants";
 import Auth from "routes/Auth";
-import Home from "routes/Home"
+import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Header from "./Header";
 import Navigation from "./Navigation";
 
 const MyRouter = () => {
-    const [isLogin, setIsLogin] = useState(false);
-    return(
-        <BrowserRouter>
-            <Header isLogin={isLogin}/>
-            <Navigation isLogin={isLogin}/>
-            <Switch>
-                <Route exact path="/">
-                    <Home isLogin={isLogin}/>
-                </Route>
-                <Route exact path="/auth">
-                    <Auth/>
-                </Route>
-                <Route exact path="/profile">
-                    <Profile/>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    )
-}
+  const [isLogin, setIsLogin] = useState(false);
+  return (
+    <BrowserRouter>
+      <div className="initialBlock">
+        <Header isLogin={isLogin} />
+        <Navigation isLogin={isLogin} />
+        <Switch>
+          <Route exact path="/">
+            <Home isLogin={isLogin} />
+          </Route>
+          <Route exact path="/auth">
+            <Auth />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/adding-plants">
+            <AddingPlants />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 export default MyRouter;
---
+++ client/src/routes/AddingPlants.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const AddingPlants = () => {
+    return(
+        <div>여기는 AddingPlants입니다.</div>
+    )
+}
+export default AddingPlants;
